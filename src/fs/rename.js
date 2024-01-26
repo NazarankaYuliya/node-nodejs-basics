e9ff82afa56e459c7a71f5eda04e93b11ac19064
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-01-26 13:25:03 +0100

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,12 @@
+import { rename as renameFile, access, constants } from "node:fs/promises";
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    await access("./files/wrongFileName.txt", constants.F_OK);
+    await renameFile("./files/wrongFileName.txt", "./files/properFilename.md");
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
