aa482eab25710867b2b2155df69f918e3e2c7f6c
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-01-26 14:13:44 +0100

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir, access, constants } from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const folder = "files";
+  try {
+    await access(folder, constants.F_OK);
+
+    try {
+      const files = await readdir(folder);
+      console.log(files);
+    } catch (error) {
+      throw new Error("FS operation failed");
+    }
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
