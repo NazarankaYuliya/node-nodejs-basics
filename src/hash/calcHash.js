c65112b8b15d8a4eb57163754b21c8cfb3f92986
Nazaranka Yuliya <nazarenko.ylia@gmail.com>
2024-01-29 16:15:19 +0100

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from "node:crypto";
+import { createReadStream } from "node:fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const stream = createReadStream("./files/fileToCalculateHashFor.txt");
+
+  stream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on("end", () => {
+    console.log(hash.digest("hex"));
+  });
+
+  stream.on("error", (error) => {
+    console.error("Error calculating hash: ", error);
+  });
 };
 
 await calculateHash();
